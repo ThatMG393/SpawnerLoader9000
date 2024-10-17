5a8f987c47e585764319d08ae2315ca7e1387fd7
ThatMG393 <thatmg393@gmail.com>
2024-10-18 07:43:58 +0800

    Initial Starting Point

---
+++ src/main/java/com/thatmg393/spawnerloader/SpawnerLoader9000DataGenerator.java
@@ -0,0 +1,11 @@
+package com.thatmg393.spawnerloader;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpawnerLoader9000DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
