48cf74c925db4a90db33522c82eff6850f411d9f
ThatMG393 <thatmg393@gmail.com>
2024-10-22 05:16:56 +0800

    Fix syntax errors

--- src/main/java/com/thatmg393/spawnerloader/block/impl/SpawnerLoaderBlock.java
+++ src/main/java/com/thatmg393/spawnerloader/block/impl/SpawnerLoaderBlock.java
@@ -6,6 +6,7 @@ import com.thatmg393.spawnerloader.SpawnerLoader9000;
 import com.thatmg393.spawnerloader.block.base.BlockExt;
 
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.Identifier;
 
