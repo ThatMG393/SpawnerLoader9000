255ac9de3149758756a8f7b87da127e34f94cda5
ThatMG393 <thatmg393@gmail.com>
2024-10-22 05:07:43 +0000

    Proper implementation of textured block and item

--- src/main/java/com/thatmg393/spawnerloader/block/impl/SpawnerLoaderBlock.java
+++ src/main/java/com/thatmg393/spawnerloader/block/impl/SpawnerLoaderBlock.java
@@ -1,22 +1,24 @@
 package com.thatmg393.spawnerloader.block.impl;
 
-import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
-import eu.pb4.polymer.core.api.block.StatelessPolymerBlock;
-
 import com.thatmg393.spawnerloader.SpawnerLoader9000;
 import com.thatmg393.spawnerloader.block.base.BlockExt;
 
+import eu.pb4.polymer.blocks.api.BlockModelType;
+import eu.pb4.polymer.blocks.api.PolymerBlockModel;
+import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
+import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
-public class SpawnerLoaderBlock extends BlockExt implements StatelessPolymerBlock, PolymerTexturedBlock {
+public class SpawnerLoaderBlock extends BlockExt implements PolymerTexturedBlock {
     public static final Identifier BLOCK_ID = Identifier.of(SpawnerLoader9000.MOD_ID, "spawner_loader_block");
-    public SpawnerLoaderBlock() {
+
+	private PolymerBlockModel polymerBlockModel = PolymerBlockModel.of(BLOCK_ID);
+
+	public SpawnerLoaderBlock() {
         super(AbstractBlock.Settings.copy(Blocks.BEACON)
 		        .allowsSpawning((state, world, pos, type) -> false)
 				.solid()
@@ -32,18 +34,7 @@ public class SpawnerLoaderBlock extends BlockExt implements StatelessPolymerBloc
     }
 
 	@Override
-	public Block getPolymerBlock(BlockState state) {
-		return getPolymerBlock(state, null);
+	public BlockState getPolymerBlockState(BlockState state) {
+		return PolymerBlockResourceUtils.requestBlock(BlockModelType.FULL_BLOCK, polymerBlockModel);
 	}
-
-	@Override
-	public Block getPolymerBlock(BlockState state, ServerPlayerEntity player) {
-		return Blocks.COBBLESTONE;
-	}
-
-	/*
-	@Override
-	public Identifier getPolymerResourceLocation() {
-		return Identifier.of(SpawnerLoader9000.MOD_ID, "block/spawner_loader_block");
-	} */
 }
--- src/main/java/com/thatmg393/spawnerloader/block/impl/blockitem/SpawnerLoaderBlockItem.java
+++ src/main/java/com/thatmg393/spawnerloader/block/impl/blockitem/SpawnerLoaderBlockItem.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.jetbrains.annotations.Nullable;
 
-import eu.pb4.polymer.core.api.item.PolymerItem;
+import com.thatmg393.spawnerloader.block.base.BlockExt;
 
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -17,10 +19,14 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Rarity;
 
 public class SpawnerLoaderBlockItem extends BlockItem implements PolymerItem {
+	private final BlockExt correlatingBlock;
+
     public SpawnerLoaderBlockItem(Block block) {
-        super(block, new Settings()
-		                .maxCount(16)
-						.rarity(Rarity.UNCOMMON));
+        super(block,
+			new Settings()
+				.maxCount(16)
+				.rarity(Rarity.UNCOMMON));
+		this.correlatingBlock = (BlockExt) block;
     }
 
 	@Override
@@ -29,13 +35,12 @@ public class SpawnerLoaderBlockItem extends BlockItem implements PolymerItem {
 	}
 
 	@Override
+	public int getPolymerCustomModelData(ItemStack itemStack, @Nullable ServerPlayerEntity player) {
+		return PolymerResourcePackUtils.requestModel(Items.BEACON, correlatingBlock.getBlockID()).value();
+	}
+
+	@Override
 	public void modifyClientTooltip(List<Text> tooltip, ItemStack stack, @Nullable ServerPlayerEntity player) {
 		tooltip.add(Text.literal("The spawner loader.").formatted(Formatting.BOLD));
 	}
-
-	/*
-	@Override
-	public Identifier getPolymerResourceLocation() {
-		return Identifier.of(SpawnerLoader9000.MOD_ID, "block/spawner_loader_block");
-	} */
 }
