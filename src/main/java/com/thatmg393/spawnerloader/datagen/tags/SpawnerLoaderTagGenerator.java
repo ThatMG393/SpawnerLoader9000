3bdd8ba6d453d19e890c0eaadf892c144e33409e
AltIsBacc <zackrondenzoolarte@gmail.com>
2024-11-02 00:53:27 +0000

    feat: generate block tags as well

---
+++ src/main/java/com/thatmg393/spawnerloader/datagen/tags/SpawnerLoaderTagGenerator.java
@@ -0,0 +1,23 @@
+package com.thatmg393.spawnerloader.datagen.tags;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.thatmg393.spawnerloader.block.registry.BlockRegistry;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper.WrapperLookup;
+import net.minecraft.registry.tag.BlockTags;
+
+public class SpawnerLoaderTagGenerator extends FabricTagProvider.BlockTagProvider {
+    public SpawnerLoaderTagGenerator(FabricDataOutput output, CompletableFuture<WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                  .add(BlockRegistry.SPAWNER_LOADER.block())
+                  .setReplace(false);
+    }
+}
