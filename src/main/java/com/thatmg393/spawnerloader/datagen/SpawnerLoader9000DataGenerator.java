3cd81d0ee153f24c8ea0cbf5bf0ec41f95fbb058
AltIsBacc <zackrondenzoolarte@gmail.com>
2024-11-02 00:53:27 +0000

    feat: generate block tags as well

--- src/main/java/com/thatmg393/spawnerloader/datagen/SpawnerLoader9000DataGenerator.java
+++ src/main/java/com/thatmg393/spawnerloader/datagen/SpawnerLoader9000DataGenerator.java
@@ -3,6 +3,7 @@ package com.thatmg393.spawnerloader.datagen;
 import com.thatmg393.spawnerloader.SpawnerLoader9000;
 import com.thatmg393.spawnerloader.datagen.loot.SpawnerLoaderLootTableGenerator;
 import com.thatmg393.spawnerloader.datagen.recipe.SpawnerLoaderRecipeGenerator;
+import com.thatmg393.spawnerloader.datagen.tags.SpawnerLoaderTagGenerator;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -13,6 +14,7 @@ public class SpawnerLoader9000DataGenerator implements DataGeneratorEntrypoint {
 		SpawnerLoader9000.LOGGER.info("my code is so cursed.");
 
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(SpawnerLoaderTagGenerator::new);
         pack.addProvider(SpawnerLoaderRecipeGenerator::new);
 		pack.addProvider(SpawnerLoaderLootTableGenerator::new);
 	}
